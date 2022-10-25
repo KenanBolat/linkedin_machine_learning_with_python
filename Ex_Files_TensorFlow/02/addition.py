a33fffc909386ca9aa98c25c6f3279f13bd51d28
Kenan Bolat <kenan.bolat@yandex.com.tr>
2022-10-25 11:00:42 +0300

    update for old tensorflow version

--- Ex_Files_TensorFlow/02/addition.py
+++ Ex_Files_TensorFlow/02/addition.py
@@ -5,15 +5,14 @@ import tensorflow as tf
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # Define computational graph
-X =
-Y =
+tf.compat.v1.disable_eager_execution()
 
-addition =
+X = tf.compat.v1.placeholder(dtype=tf.float32, name='X')
+Y = tf.compat.v1.placeholder(dtype=tf.float32, name='Y')
 
+addition = tf.add(X, Y, name='Addition')
 
 # Create the session
-with tf.Session() as session:
-
-    result =
-
+with tf.compat.v1.Session() as session:
+    result = session.run(addition, feed_dict={X: [1, 1, 1, 2, 3], Y: [4, 4, 4, 5, 6]})
     print(result)
